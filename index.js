c417b70fece77d5ad7eae3fbe4b16c1d1c61db96
Keith Cirkel <keithamus@users.noreply.github.com>
2016-04-22 16:37:29 +0100

    feat: add declaration-block-no-ignored-properties rule
    BREAKING CHANGE:
    
    Any block which has a display property that conflicts with other properties will now fail lint

--- index.js
+++ index.js
@@ -150,6 +150,8 @@ module.exports = {
     // # Declaration block
     // Disallow duplicate properties within declaration blocks.
     'declaration-block-no-duplicate-properties': true,
+    // Disallow property values that are ignored due to another property value in the same rule.
+    'declaration-block-no-ignored-properties': true,
     // Disallow shorthand properties that override related longhand properties within declaration blocks.
     'declaration-block-no-shorthand-property-overrides': true,
     // Specify the order of properties within declaration blocks.
